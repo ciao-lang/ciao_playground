cea5e7c8510c3c0541f3ecba9597e2ef0ca097f6
Jose F. Morales <jfmcjf@gmail.com>
2023-04-26 16:04:07 +0200

    (ciao_playground) build.sh: stop if emcc is not found
    Src-commit: 4e8554ca22c60040eba133ca02b2934f8750c595

--- build.sh
+++ build.sh
@@ -23,6 +23,23 @@ EOF
     exit 1
 fi
 
+if ! [ -x "$(command -v emcc)" ]; then
+    cat <<EOF
+ERROR: Could not find 'emcc' in the path. 
+
+Please install and activate emscripten:
+
+  https://emscripten.org/docs/getting_started/downloads.html
+
+Make sure that your installation is enabled in your current shell with:
+
+  # (from your emsdk directory)
+  source ./emsdk_env.sh
+
+EOF
+    exit 1
+fi
+
 # ---------------------------------------------------------------------------
 # Install ciaowasm and the playground 
 
