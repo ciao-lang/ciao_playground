11a9168f8691ead0a77d7cd994fa15a140f5bbf3
Jose F. Morales <jfmcjf@gmail.com>
2023-05-01 11:21:46 +0200

    (ciao_playground) allow CORS in local server
    Src-commit: 767a5524dd62c7fbc3a8c005af95867b877e6f8f

--- etc/mt-httpd.py
+++ etc/mt-httpd.py
@@ -1,8 +1,22 @@
 import sys
 import http.server
 
+# (Allow CORS)
+class CORSRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', '*')
+        self.send_header('Access-Control-Allow-Headers', '*')
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
+        return super(CORSRequestHandler, self).end_headers()
+
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.end_headers()
+
 def serve(port):
-    handler = http.server.SimpleHTTPRequestHandler
+    # handler = http.server.SimpleHTTPRequestHandler
+    handler = CORSRequestHandler
     handler.extensions_map.update({
         '.wasm': 'application/wasm' # still not official
     })
